de4535cfe7870a88ac01cd09bbd451c5bf2d4b03
Vijay Kandy <vkandy@users.noreply.github.com>
2017-11-16 14:09:29 -0700

    migrate using private keys
    -- added wallet provider to help migrate using private keys

--- truffle.js
+++ truffle.js
@@ -1,16 +1,39 @@
-const fs               = require('fs');
-const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs               = require("fs");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const ethereumjsWallet  = require("ethereumjs-wallet");
+const ProviderEngine    = require("web3-provider-engine");
+const WalletSubprovider = require("web3-provider-engine/subproviders/wallet.js");
+const Web3Subprovider   = require("web3-provider-engine/subproviders/web3.js");
+const Web3              = require("web3");
+const FilterSubprovider = require("web3-provider-engine/subproviders/filters.js");
 
 let secrets;
 let mnemonic;
+let wallet;
+let privateKey;
+let address;
 
-if(fs.existsSync('secrets.json')) {
-    secrets  = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
-    mnemonic = secrets.mnemonic;
+if(fs.existsSync("secrets.json")) {
+    secrets    = JSON.parse(fs.readFileSync("secrets.json", "utf8"));
+    mnemonic   = secrets.mnemonic;
+    privateKey = secrets.privateKey;
+    wallet     = ethereumjsWallet.fromPrivateKey(new Buffer(privateKey, "hex"));
+    address    = "0x" + wallet.getAddress().toString("hex");
 } else {
-    console.log('no secrets.json found. You can only deploy to the testrpc.');
-    mnemonic = '';
+    console.log("no secrets.json found. You can only deploy to the testrpc.");
+    mnemonic   = "";
+    privateKey = "";
 }
+
+let initProvider = (providerUrl) => {
+    let engine = new ProviderEngine();
+    engine.addProvider(new FilterSubprovider());
+    engine.addProvider(new WalletSubprovider(wallet, {}));
+    engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(providerUrl)));
+    engine.start();
+    return engine;
+};
 
 module.exports = {
     networks: {
@@ -18,13 +41,31 @@ module.exports = {
             host: "localhost",
             port: 8545,
             gas: 4712388,
-            network_id: "*" // Match any network id
+            network_id: "*"
         },
-        kovan: {
-            provider: new HDWalletProvider(mnemonic, 'https://kovan.infura.io'),
+        ropsten: {
+            provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/[key]'),
             network_id: '*',
             gas: 4500000,
             gasPrice: 25000000000,
+        },
+        kovan: {
+            provider: function() {
+                return initProvider("https://kovan.infura.io/[key]");
+            },
+            from: address,
+            network_id: "*",
+            gas: 4500000,
+            gasPrice: 25000000000,
+        },
+        rinkeby: {
+            provider: function() {
+                return initProvider("https://rinkeby.infura.io/[key]");
+            },
+            from: address,
+            network_id: "*",
+            gas: 4500000,
+            gasPrice: 25000000000
         }
     }
 };
