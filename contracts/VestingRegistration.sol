afc4a065754e731b8ca3462234435fc8705e86b1
lsaether <Lsaether@protonmail.com>
2017-11-24 12:14:18 -0700

    staging

---
+++ contracts/VestingRegistration.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.18;
+
+import "./Vesting.sol";
+
+library VestingRegistration {
+
+    function EighteenMonthVest(address _vestingWallet,
+                               address _beneficiary)
+    {
+        
+    }
+}
